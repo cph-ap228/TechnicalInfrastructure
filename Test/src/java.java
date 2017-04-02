90f42e15a01ba1438b706d429aa8f97142052b89
Farki93 <cph-if36@cphbusiness.dk>
2017-04-02 15:14:09 +0200

    added a class

---
+++ Test/src/java.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Farkas on 4/2/2017.
+ */
+import java.util.Scanner;
+
+public class java
+{
+
+ public String name;
+
+ public void start(){
+
+
+    Scanner in = new Scanner(System.in);
+    System.out.println("Welcome to a simple app... \n"
+            + " Enter your name:");
+    name = in.next();
+}
+}
